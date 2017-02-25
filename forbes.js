fef365a7ec450368d7e9c1cc7216ccc10a8a80d0
Francis Larson <fsnlarson@gmail.com>
2017-02-25 13:17:52 +0000

    data to 2006

--- forbes.js
+++ forbes.js
@@ -4,7 +4,7 @@ var csvWriter = require('csv-write-stream')
 var fs = require('fs')
 
 
-var years = [2010, 2011, 2012, 2013, 2014, 2015, 2016].reverse();
+var years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016].reverse();
 
 function retrieve(query, callback) {
     var data = {
